b2f05d5f357d2849397cb7cb19723dad7bf779ed
Irina <irstrashuk@gmail.com>
2017-01-21 17:53:29 +0200

    Hendalf appears again

--- js/stat.js
+++ js/stat.js
@@ -1,19 +1,24 @@
-var canvas = document.querySelector('canvas');
-var ctx = canvas.getContext('2d');
+ 'use strict';
 
-var names = ['Вы', 'Игорь', 'Миша', 'Петр', 'Коля'];
-var times = [20, 30, 40, 50, 60];
-
-function renderStatistics(ctx, names, times) {
-  // body...
-}
-
+window.renderStatistics = function(ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
 
-  ctx.fillStyle = 'white';
+  ctx.fillStyle = 'rgba(256, 256, 256, 1.0)';
+  ctx.strokeRect(100, 10, 420, 270);
   ctx.fillRect(100, 10, 420, 270);
+}
 
-  ctx.fillText('Ура вы победили!');
-  ctx.fillText('Список результатов:');
-  ctx.font('16px PT Mono');
+var canvas = document.querySelector('canvas');
+
+window.renderStatistics(
+  canvas.getContext('2d');
+  ['Вы', 'Игорь', 'Миша', 'Петр', 'Коля'];
+  [20, 30, 40, 50, 60];
+  )
+
+
+//     ctx.textBaseline = 'hanging';
+//     ctx.fillText('Ура вы победили!', 20, 10);
+//     ctx.fillText('Список результатов:', 20, 48);
+//     ctx.font('16px PT Mono');
