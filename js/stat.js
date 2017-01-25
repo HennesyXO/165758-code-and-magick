637ed9c344b43a9dbadd5957d7b0eccc55b11d91
Irina <irstrashuk@gmail.com>
2017-01-25 20:59:18 +0200

    Correction on variable time, X coordinate, avoiding code repetition

--- js/stat.js
+++ js/stat.js
@@ -12,8 +12,8 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = '#000';
   ctx.textBaseline = 'hanging';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Слава победителю!', 150, 20);
-  ctx.fillText('Вот результаты:', 150, 40);
+  ctx.fillText('Слава победителю!', 240, 20);
+  ctx.fillText('Кто молодец? Вы молодец!', 200, 40);
 
   var max = -1;
 // цикл который найдет текущее максимальное значение из массива
@@ -24,28 +24,32 @@ window.renderStatistics = function (ctx, names, times) {
     }
   }
 // гистограмма с параметрами столбиков
-  var histoHeight = 150;
-  var histoTopOffset = 70;
-  var columnWidth = 40;
-  var dropWidth = 50;
-  var step = histoHeight / max;
+  var histoHeight = 150;// высота области ниже текста
+  var leftOffset = 150;
+  var histoTopOffset = 90;// отступ сверху
+  var columnWidth = 40;// ширина колонки
+  var dropWidth = 50;// просветы меж колонками
+  var step = histoHeight / max; // цена деления
   // цикл для рисования столбиков по именам игроков и времени
   for (i = 0; i < times.length; i++) {
     var name = names[i];
-    var height = step * time;
-    var histoY = histoHeight - height + histoTopOffset;
+    var seconds = times[i];
+    var columnHeight = step * seconds;
+    var histoX = leftOffset + ((columnWidth + dropWidth) * i);
+    var histoY = histoHeight - columnHeight + histoTopOffset;
 
     if (name === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      ctx.fillRect(histoHeight + dropWidth * i, histoY, columnWidth, height);
     } else {
 // функция, случайным образом генерирует оттенки синего
       ctx.fillStyle = 'rgb(0,' + '0' + ',' + (Math.random() * 255).toFixed(0) + ')';
-      ctx.fillRect(histoHeight + dropWidth * i, histoY, columnWidth, height);
     }
-// выводит имена участников и время
-    ctx.fillStyle = '#000';
-    ctx.fillText(name + ':', histoHeight + dropWidth * i, histoY + height + 10);
-    ctx.fillText(time.toFixed(0) + ' ', histoHeight + dropWidth * i, histoY + height + 30);
+
+    ctx.fillRect(histoX, histoY, columnWidth, columnHeight);
+
+    // выводит имена участников и время
+    ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+    ctx.fillText(name + ' ', histoX, histoY + columnHeight + 10);
+    ctx.fillText(seconds.toFixed(0) + ' ', histoX, histoY - 20);
   }
 };
