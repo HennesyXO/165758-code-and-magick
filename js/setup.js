de23147512919b48069e1e78d0823409339bf5e8
Irina <irstrashuk@gmail.com>
2017-01-30 15:42:12 +0200

    spaces corrections

--- js/setup.js
+++ js/setup.js
@@ -21,12 +21,12 @@ setupUserName.value = '';
 var wizard = document.querySelector('#wizard');
 var wizardCoat = wizard.querySelector('#wizard-coat');
 var coatColors = [
-'rgb(101, 137, 164)',
-'rgb(241, 43, 107)',
-'rgb(146, 100, 161)',
-'rgb(56, 159, 117)',
-'rgb(215, 210, 55)',
-'rgb(0, 0, 0)'
+  'rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(146, 100, 161)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'
 ];
 
 wizardCoat.addEventListener('click', function () {
@@ -36,11 +36,11 @@ wizardCoat.addEventListener('click', function () {
 
 var wizardEyes = document.querySelector('#wizard-eyes');
 var eyeColors = [
-'black',
-'red',
-'blue',
-'yellow',
-'green'
+  'black',
+  'red',
+  'blue',
+  'yellow',
+  'green'
 ];
 
 wizardEyes.addEventListener('click', function () {
@@ -50,11 +50,11 @@ wizardEyes.addEventListener('click', function () {
 
 var fireball = document.querySelector('.setup-fireball-wrap');
 var fireballColors = [
-'#ee4830',
-'#30a8ee',
-'#5ce6c0',
-'#e848d5',
-'#e6e848'
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
 ];
 
 fireball.addEventListener('click', function () {
